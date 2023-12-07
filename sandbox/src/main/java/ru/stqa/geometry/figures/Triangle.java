3de461e77d22eb395711de12c9e30248a08deb10
ruslan73rus <ruslan73rus@mail.ru>
2023-12-07 11:34:45 +0300

    Сравнение объектов

--- sandbox/src/main/java/ru/stqa/geometry/figures/Triangle.java
+++ sandbox/src/main/java/ru/stqa/geometry/figures/Triangle.java
@@ -1,5 +1,7 @@
 package ru.stqa.geometry.figures;
 
+import java.util.Objects;
+
 public record Triangle (
     double aSide,
     double bSide,
@@ -28,4 +30,22 @@ public record Triangle (
     public double area()
     {double p = (this.perimeter()) / 2;
         return Math.sqrt(p * (p - this.aSide) * (p - this.bSide) * (p - this.cSide)); }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Triangle triangle = (Triangle) o;
+        return (Double.compare(this.aSide, triangle.aSide) == 0 && Double.compare(this.bSide, triangle.bSide) == 0 && Double.compare(this.cSide, triangle.cSide) == 0)
+                || (Double.compare(this.bSide, triangle.aSide) == 0 && Double.compare(this.aSide, triangle.bSide) == 0 && Double.compare(this.cSide, triangle.cSide) == 0)
+                || (Double.compare(this.cSide, triangle.aSide) == 0 && Double.compare(this.bSide, triangle.bSide) == 0 && Double.compare(this.aSide, triangle.cSide) == 0)
+                || (Double.compare(this.aSide, triangle.aSide) == 0 && Double.compare(this.cSide, triangle.bSide) == 0 && Double.compare(this.bSide, triangle.cSide) == 0)
+                || (Double.compare(this.bSide, triangle.aSide) == 0 && Double.compare(this.cSide, triangle.bSide) == 0 && Double.compare(this.aSide, triangle.cSide) == 0)
+                || (Double.compare(this.cSide, triangle.aSide) == 0 && Double.compare(this.aSide, triangle.bSide) == 0 && Double.compare(this.bSide, triangle.cSide) == 0);
     }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(aSide, bSide, cSide);
+    }
+}
